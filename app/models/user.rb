d51e8c3e9e11d94a7c41330d0b20cce3f8da55cd
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-02-13 13:09:49 +0100

    first part

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+
+  has_many :gossips, dependent: :destroy
+  
+  # attr_accessor :sign_up_code
+  # validates :sign_up_code,
+    # on: :create,
+    # presence: true,
+    # inclusion: { in: ["GOSSIP2017"] }
+
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
